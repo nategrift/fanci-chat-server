cb17c046815f42a48bb412a5a101afe01ca415c7
nategrift <nategrift@gmail.com>
2020-11-22 23:52:10 -0500

    added youtube hidden theatre

---
+++ data/theatres.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        icon: "users",
+        roomName: "Public #1",
+        numberOnline: 0,
+        hasSheild: true,
+        roomID: "public1",
+        private: false,
+    },
+    {
+        icon: "book",
+        roomName: "Public #2",
+        numberOnline: 0,
+        hasSheild: true,
+        roomID: "public2",
+        private: false,
+    },
+    {
+        icon: "icons",
+        roomName: "Public #3",
+        numberOnline: 0,
+        hasSheild: true,
+        roomID: "public3",
+        private: false,
+    },
+    {
+        icon: "gamepad",
+        roomName: "Public #4",
+        numberOnline: 0,
+        hasSheild: true,
+        roomID: "public4",
+        private: false,
+    },
+];
