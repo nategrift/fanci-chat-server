03c6b01cce960add793109f27e97396e5b33b859
nategrift <nategrift@gmail.com>
2021-02-02 16:17:37 -0500

    Added finished mobile support v1

---
+++ client/build/precache-manifest.8f60f7200343a187d369088c5f06855c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6998bf978214221795ccc0add61e6aca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02b4bf83a4476821342e",
+    "url": "/static/css/main.38450a34.chunk.css"
+  },
+  {
+    "revision": "e60fcc8e4bc5da410f01",
+    "url": "/static/js/2.f367378c.chunk.js"
+  },
+  {
+    "revision": "0dc7f0eecb5e9289945db6a17e48a2f1",
+    "url": "/static/js/2.f367378c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b4bf83a4476821342e",
+    "url": "/static/js/main.3040dece.chunk.js"
+  },
+  {
+    "revision": "a23ed385cbf80c575a9b",
+    "url": "/static/js/runtime-main.8cca2583.js"
+  },
+  {
+    "revision": "7decaf064c4be52f9745d642318943ac",
+    "url": "/static/media/add-icon.7decaf06.svg"
+  },
+  {
+    "revision": "b7674fa175f8dd723dda4ec449782ae9",
+    "url": "/static/media/home-graphic.b7674fa1.png"
+  },
+  {
+    "revision": "180800f19ebddcfca8f697070bb7b14f",
+    "url": "/static/media/nate-profile.180800f1.png"
+  },
+  {
+    "revision": "313f1b710c78fe1ff567865f23517e75",
+    "url": "/static/media/send-icon.313f1b71.svg"
+  }
+]);
